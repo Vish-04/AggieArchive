e8f1f0bf75a9585cf55750ff2b7c5e20cc0c28a4
VishReddy <vishwareddy@gmail.com>
2023-05-20 13:19:21 -0700

    added recoil adn routing to app.js

--- aggie-exchange/src/App.jsx
+++ aggie-exchange/src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import {RecoilRoot} from "recoil";
+
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <RecoilRoot>
+      <Router>
+        
+        
+        <Routes>
+          
+        </Routes>
+      </Router>
+    </RecoilRoot>
   )
 }
 
