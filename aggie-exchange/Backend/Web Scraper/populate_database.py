186a7a60c0f1c9056d381e735cc1268e2f0aae14
VishReddy <vishwareddy@gmail.com>
2023-05-20 20:48:11 -0700

    bugfix

--- aggie-exchange/Backend/Web Scraper/populate_database.py
+++ aggie-exchange/Backend/Web Scraper/populate_database.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 
-with open("vite-super-secret\Backend\Web Scraper\data\course.json", "r") as f:
+with open("AggieArchive\\aggie-exchange\Backend\Web Scraper\data\course.json", "r") as f:
     courses = json.load(f)
 
 url = "http://localhost:3000/courses/add"
